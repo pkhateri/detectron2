5841a9120dea8630f36565ef2214312cf8e403db
Parisa Khateri <parisa.khateri@gmail.com>
2022-11-29 15:11:37 +0100

    rename and add inference for ballon test

--- datasets/prepare_ballon_dataset_train_simple_model_inference.py
+++ datasets/prepare_ballon_dataset_train_simple_model_inference.py
@@ -1,6 +1,7 @@
 '''
 1- prepare ballon dataset 
 2- fine-tune a COCO-pretrained R50-FPN Mask R-CNN model on the balloon dataset
+3- infere and evaluate
 '''
 
 # Some basic setup:
@@ -91,3 +92,31 @@ os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
 trainer = DefaultTrainer(cfg) 
 trainer.resume_or_load(resume=False)
 trainer.train()
+
+# inference and evaluation
+# Inference should use the config with parameters that are used in training
+# cfg now already contains everything we've set previously. We changed it a little bit for inference:
+cfg.MODEL.WEIGHTS = os.path.join(cfg.OUTPUT_DIR, "model_final.pth")  # path to the model we just trained
+cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.7   # set a custom testing threshold
+predictor = DefaultPredictor(cfg)
+
+from detectron2.utils.visualizer import ColorMode
+dataset_dicts = get_balloon_dicts("balloon/val")
+for d in random.sample(dataset_dicts, 3):    
+    im = cv2.imread(d["file_name"])
+    outputs = predictor(im)  # format is documented at https://detectron2.readthedocs.io/tutorials/models.html#model-output-format
+    v = Visualizer(im[:, :, ::-1],
+                   metadata=balloon_metadata, 
+                   scale=0.5, 
+                   instance_mode=ColorMode.IMAGE_BW   # remove the colors of unsegmented pixels. This option is only available for segmentation models
+    )
+    out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
+    cv2_imshow(out.get_image()[:, :, ::-1])
+
+    
+from detectron2.evaluation import COCOEvaluator, inference_on_dataset
+from detectron2.data import build_detection_test_loader
+evaluator = COCOEvaluator("balloon_val", output_dir="./output")
+val_loader = build_detection_test_loader(cfg, "balloon_val")
+print(inference_on_dataset(predictor.model, val_loader, evaluator))
+# another equivalent way to evaluate the model is to use `trainer.test`
